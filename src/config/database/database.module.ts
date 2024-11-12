6da4d84a56ed76ba185e79726e3a8084864ab085
Daniyal-Lodhi <daniyallodhi25@gmail.com>
2024-11-13 02:31:28 +0500

    merge

--- src/config/database/database.module.ts
+++ src/config/database/database.module.ts
@@ -4,26 +4,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { dbConFail, dbConSuccess } from 'src/common/consts/db-const';
 import { testUserEntity } from 'src/modules/test/entities/test.entity';
+import { dataSourceOptions } from './typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
-        const config: TypeOrmModuleOptions = {
-          type: 'postgres',
-          host: configService.get<string>('POSTGRES_HOST'),
-          port: configService.get('POSTGRES_PORT'),
-          username: configService.get<string>('POSTGRES_USER'),
-          password: configService.get<string>('POSTGRES_PASSWORD'),
-          database: configService.get<string>('POSTGRES_DB'),
-          schema: configService.get<string>('open gangum style'), 
-          entities: [__dirname + '/../**/*.entity{.ts,.js}',testUserEntity],
-          synchronize: true,
-        };
+      useFactory: async (): Promise<TypeOrmModuleOptions> => {
+        const config: TypeOrmModuleOptions = dataSourceOptions as TypeOrmModuleOptions
 
-        // Cast config to DataSourceOptions for manual initialization
         const dataSource = new DataSource(config as DataSourceOptions);
         try {
           await dataSource.initialize();
