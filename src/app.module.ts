b334908eed60e8f3d4c2094aa309a97401e3cd59
Daniyal-Lodhi <daniyallodhi25@gmail.com>
2024-11-17 21:05:06 +0500

    (feat): adds general user's register, login, get, update and delete api.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { envFilePath } from './config/env.config';
-import { DatabaseModule } from './config/database/database.module';
+ import { DatabaseModule } from './config/database/database.module';
 import { TestModule } from './modules/test/test.module';
+import { UserController } from './modules/user/user.controller';
+import { UserService } from './modules/user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/entities/user.entity';
+import { LocalAuthService } from './modules/auth/local-auth/local-auth.service';
+import { UserModule } from './modules/user/user.module';
+import { LocalAuthModule } from './modules/auth/local-auth/local-auth.module';
+import { ConfigModule } from './config/config.module';
+import { CookieExtractorMiddleware } from './common/middleware/cookie-extractor.middleware';
+import * as cookieParser from 'cookie-parser';
 
 
 
 
 @Module({
   imports: [ 
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath,
-    }),
+    ConfigModule,
     DatabaseModule,
     TestModule,
+    TypeOrmModule.forFeature([User]) ,
+    LocalAuthModule,
+    UserModule,
+
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController,UserController],
+  providers: [AppService,UserService,LocalAuthService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieParser(), CookieExtractorMiddleware).forRoutes('*');
+}
+ }
