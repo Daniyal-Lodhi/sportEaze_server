e9ec73d7f3255edec13cbd292f54cdb6a95d5e14
Zohaib Ali <zohaib.ali236@gmail.com>
2025-03-08 23:31:01 +0500

    adds swagger documentation to post-like DTO

--- src/modules/user/user-posts/post-likes/dto/post-like.dto.ts
+++ src/modules/user/user-posts/post-likes/dto/post-like.dto.ts
@@ -1,11 +1,24 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsOptional, IsBoolean } from 'class-validator';
 import { ReactTypeEnum } from 'src/common/enums/user-posts.enum';
 
 export class LikePostDto {
+  @ApiPropertyOptional({
+    enum: ReactTypeEnum,
+    enumName: 'ReactTypeEnum',
+    description: 'Type of reaction. Required when unLike is false.',
+    example: ReactTypeEnum.HEART
+  })
   @IsEnum(ReactTypeEnum)
   @IsNotEmpty()
   reactType?: ReactTypeEnum;
 
+  @ApiProperty({
+    type: Boolean,
+    required: true,
+    description: 'Indicates if the user wants to remove the like. This field is required.',
+    example: false
+  })
   @IsBoolean()
   @IsNotEmpty()
   unLike: boolean ; // Default to false if not provided
