de1c97c397b1b003837c7eec9d6c9b3a854c9ed3
Zohaib Ali <zohaib.ali236@gmail.com>
2025-03-26 07:50:47 +0500

    add enum type for connection status in migration

--- src/config/database/migrations/1742684683460-network-1.ts
+++ src/config/database/migrations/1742684683460-network-1.ts
@@ -4,6 +4,7 @@ export class Network11742684683460 implements MigrationInterface {
     name = 'Network11742684683460'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "sportEaze"."Connections_status_enum" AS ENUM('1', '2', '3')`);
         await queryRunner.query(`CREATE TABLE "sportEaze"."Followers" ("id" uuid NOT NULL, "playerId" uuid NOT NULL, "followerId" uuid NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "REL_e23570f14dc2e46a348ac0618f" UNIQUE ("playerId"), CONSTRAINT "REL_cccee741c1cf2e3dfe04a00b1f" UNIQUE ("followerId"), CONSTRAINT "PK_6d71b92abdd180a7d9859d75551" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "sportEaze"."Connections" ("id" uuid NOT NULL, "senderId" uuid NOT NULL, "receiverId" uuid NOT NULL, "status" "sportEaze"."Connections_status_enum" NOT NULL DEFAULT '1', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "REL_afc8205a0693827a5c269e2116" UNIQUE ("senderId"), CONSTRAINT "REL_02987a11281e42d09924ff1aa5" UNIQUE ("receiverId"), CONSTRAINT "PK_0ee1f6fd09d463a63ca7cded8b3" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "sportEaze"."Followers" ADD CONSTRAINT "FK_e23570f14dc2e46a348ac0618fe" FOREIGN KEY ("playerId") REFERENCES "sportEaze"."Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
