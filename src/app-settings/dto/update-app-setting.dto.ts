fa6b707816c410a759e558b4b7895a7b23c94d82
Zohaib Ali <zohaib.ali236@gmail.com>
2025-04-27 01:42:20 +0500

    feat: enhance UpdateAppSettingDto with detailed ApiProperty descriptions for user settings

--- src/app-settings/dto/update-app-setting.dto.ts
+++ src/app-settings/dto/update-app-setting.dto.ts
@@ -1,6 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean } from 'class-validator';
 
 export class UpdateAppSettingDto {
+    @ApiProperty({
+        description: 'Specifies if users are allowed to be deleted',
+        example: true,
+    })
+    @IsBoolean()
     allowDeleteUser: boolean;
+    
+    @ApiProperty({
+        description: 'Specifies if users are allowed to update their information',
+        example: true,
+    })
+    @IsBoolean()
     allowUpdateUser: boolean;
+    
+    @ApiProperty({
+        description: 'Specifies if the system should take user consent',
+        example: false,
+    })
+    @IsBoolean()
     shouldTakeConsent: boolean;
 }
