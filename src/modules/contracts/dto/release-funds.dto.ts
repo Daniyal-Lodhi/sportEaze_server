eea25ed8e13f7e1fa4efefda720fc8ce80e83d35
Zohaib Ali <zohaib.ali236@gmail.com>
2025-05-13 02:46:40 +0500

    feat: add ApiProperty decorators to playerId and milestoneId in ReleaseFundsDto

--- src/modules/contracts/dto/release-funds.dto.ts
+++ src/modules/contracts/dto/release-funds.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class ReleaseFundsDto { 
+    @ApiProperty()
     @IsString()
     playerId: string;
-
+    
+    @ApiProperty()
     @IsString()
     milestoneId: string;
 }
